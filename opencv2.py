c4458da22d45d80dddb43d321875a2fe3d0f740c
X-TRON404 <55325140+X-TRON404@users.noreply.github.com>
2021-08-22 12:59:02 +0530

    Read Videos

--- opencv2.py
+++ opencv2.py
@@ -8,6 +8,8 @@ cap=cv2.VideoCapture(0)
 
 while True:
     retrn,frame=cap.read()
+    #frame = current frame
+    #retrn = got the frame(True) or not(False)
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
     cv2.imshow('gray',gray)
